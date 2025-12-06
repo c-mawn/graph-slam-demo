3d126db54ee423c45780835edec214e459a8310d
cmawn <cmawn@olin.edu>
2025-12-06 14:37:46 -0500

    pose graph working with full dataset

--- graph/pose_graph.py
+++ graph/pose_graph.py
@@ -2,17 +2,22 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 from math import pi
 from pose_stamped import poseStamped
 from gen_edge import sensor_edge
+from helper_functions import pose_from_odom, unpack_bearing_range
 
 
 class poseGraph:
     def __init__(
         self,
-        sensor_table: list[
-            float, list[float, float, float], list[float, float], list[float, float]
-        ],
+        sensor_table: (
+            list[
+                float, list[float, float, float], list[float, float], list[float, float]
+            ]
+            | pd.DataFrame
+        ),
     ):
         self.sensor_table = sensor_table
         self.graph = nx.Graph()
@@ -27,50 +32,69 @@ class poseGraph:
 
         prev_node = None
 
-        for row in self.sensor_table:
-            # [0]: time, [1]: pose [x, y, theta], [2]: beacon [dist, bearing], [3]: odom [lin_v, ang_v]
-            timestep = row[0]
-            x, y, theta = row[1]
+        table_col = self.sensor_table.columns
+        beacon_count = 0
+        for col in table_col:
+            beacon_count += (
+                1 if re.search(re.escape("LandmarkPinger_Landmark"), col) else 0
+            )
 
-            # creates a node
-            current_node = poseStamped((x, y), theta, timestep)
+        for index, row in self.sensor_table.iterrows():
+            # goes thru each row in table
+            # [0] index, [1]: time, [2:5]: beacons, [6]: gps, [7,8]: odom, [9,10], cmd vel
+            timestamp = row["Time"]
+            # getting pose estimate
+            if index == 1 or index == 0:
+                x, y, theta = (0.0, 0.0, 0.0)
+            else:
+                # TODO: non constant dt (how to generate it?)
+                updated_pose = pose_from_odom(
+                    poseStamped((x, y), theta, timestamp),
+                    0.1,
+                    row["Odometry_LinearVelocity"],
+                    row["Odometry_AngularVelocity"],
+                )
+                x, y = updated_pose.position
+                theta = updated_pose.orientation
 
-            # We use the node object itself as the key in networkx
-            self.graph.add_node(current_node, pos=(x, y))
+            # creates a new node
+            current_node = poseStamped((x, y), theta, timestamp)
+            self.graph.add_node(current_node, pos=(x, y), node_type="odom")
 
-            # 2. Add odom edge constraint
+            # connecting pose nodes
             if prev_node is not None:
                 # Extract odom data if available
-                lin_v, ang_v = row[3] if len(row) > 3 else (0.0, 0.0)
+                lin_v = row["Odometry_LinearVelocity"]
+                ang_v = row["Odometry_AngularVelocity"]
 
-                # Create the edge dict
+                # create the edge dict
                 odom_edge = sensor_edge(
-                    time=timestep, type="odom", b=None, r=None, lv=lin_v, av=ang_v
+                    time=timestamp, type="odom", b=None, r=None, lv=lin_v, av=ang_v
                 )
 
                 # connect previous node to current node
                 self.graph.add_edge(prev_node, current_node, **odom_edge)
 
             # Check if beacon data exists and is valid
-            if row[2] and row[2][0] is not None:
-                r = row[2][0]
-                b = row[2][1]
 
-                # calculate absolute position of beacon
-                x_b = x + r * np.cos(theta + b)
-                y_b = y + r * np.sin(theta + b)
+            for i in range(beacon_count):
+                b, r = unpack_bearing_range(row[f"LandmarkPinger_Landmark{i}"])
+                if b is not None and r is not None:
+                    # calculate absolute position of beacon
+                    x_b = x + r * np.cos(theta + b)
+                    y_b = y + r * np.sin(theta + b)
 
-                beacon_node = poseStamped((x_b, y_b), 0.0, timestep)
+                    beacon_node = poseStamped((x_b, y_b), 0.0, timestamp)
 
-                # Add beacon node
-                self.graph.add_node(beacon_node, pos=(x_b, y_b))
+                    # Add beacon node
+                    self.graph.add_node(beacon_node, pos=(x_b, y_b), node_type="beacon")
 
-                # Create edge between current pose and beacon
-                beacon_edge_attr = sensor_edge(
-                    time=timestep, type="beacon", r=r, b=b, lv=None, av=None
-                )
+                    # Create edge between current pose and beacon
+                    beacon_edge_attr = sensor_edge(
+                        time=timestamp, type="beacon", r=r, b=b, lv=None, av=None
+                    )
 
-                self.graph.add_edge(current_node, beacon_node, **beacon_edge_attr)
+                    self.graph.add_edge(current_node, beacon_node, **beacon_edge_attr)
 
             # Update previous node for the next iteration
             prev_node = current_node
@@ -107,21 +131,41 @@ class poseGraph:
             label="Beacon",
         )
 
+        all_nodes = self.graph.nodes(data=True)
+        odom_nodes = [n for n, d in all_nodes if d.get("node_type") == "odom"]
+        beacon_nodes = [n for n, d in all_nodes if d.get("node_type") == "beacon"]
+
+        nx.draw_networkx_nodes(
+            self.graph,
+            pos_layout,
+            nodelist=odom_nodes,
+            node_size=5,
+            node_color="blue",
+            ax=ax,
+            label="Robot Pose",
+        )
+
+        nx.draw_networkx_nodes(
+            self.graph,
+            pos_layout,
+            nodelist=beacon_nodes,
+            node_size=5,
+            node_color="green",
+            node_shape="s",
+            ax=ax,
+            label="Landmark",
+        )
+
         X, Y, U, V = [], [], [], []
 
-        for node in self.graph.nodes():
+        for node in odom_nodes:
             X.append(node.position[0])
             Y.append(node.position[1])
-            # beacon nodes might define orientation as 0, which is fine
             U.append(np.cos(node.orientation))
             V.append(np.sin(node.orientation))
 
         # draw robot poses as arrows
-        ax.quiver(X, Y, U, V, pivot="mid", color="blue", scale=20, headwidth=5)
-
-        nx.draw_networkx_nodes(
-            self.graph, pos_layout, node_size=20, node_color="blue", ax=ax
-        )
+        ax.quiver(X, Y, U, V, pivot="mid", color="blue", scale=50, headwidth=4)
 
         ax.set_aspect("equal")
         plt.legend()
@@ -139,9 +183,11 @@ exmaple_sensor_table = [
     [2.5, [1.0, 1.0, pi / 2], [np.sqrt(0.5), 3 * pi / 4], [0.0, 0.0]],
 ]
 
+df = pd.read_csv("graph/sensor_log.csv")
+
 
 def main():
-    pg = poseGraph(exmaple_sensor_table)
+    pg = poseGraph(df)
 
 
 if __name__ == "__main__":
